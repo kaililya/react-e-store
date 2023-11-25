3f8f1d1a08a52404c8d89e86574d039769a917eb
kail <ilyaman.zona2033@mail.ru>
2023-11-25 21:18:43 +0500

    final_version_no_import_10

--- src/pages/good-item-page/good-item-page.tsx
+++ src/pages/good-item-page/good-item-page.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './good-item-page.module.css'
 import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks';
 import { fetchCurrentGood } from '../../services/thunks/thunks';
